d8fafd882eebcb6a398310243ecef68b0b7d6ebf
santhosh-kumar1928 <santhoshkumar.p2024eee@sece.ac.in>
2026-07-29 09:24:53 +0530

    Delete server directory

--- server/knowledgeBase.js
+++
@@ -1,8 +0,0 @@
-/**
- * Placement Intelligence Knowledge Base
- * Contains target requirements for major tech companies and core engineering roles.
- */
-import { COMPANY_REQUIREMENTS as CLIENT_COMPANIES, ROLE_REQUIREMENTS as CLIENT_ROLES } from '../src/data/knowledgeData.js';
-
-export const COMPANY_REQUIREMENTS = CLIENT_COMPANIES;
-export const ROLE_REQUIREMENTS = CLIENT_ROLES;
